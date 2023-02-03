5469167ba00b0c2e35c81b813800c37d21fbee1b
Maxim Urschumzew <u.maxim@live.de>
2023-02-03 18:16:35 +0100

    Initial commit.

---
+++ flake.nix
@@ -0,0 +1,22 @@
+
+{
+  description = "A basic flake with a shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive pkgs.pkg-config ];
+        buildInputs = with pkgs; [
+          # maturin
+          # poetry
+          # python39Packages.pipx
+          openssl
+          # postgresql_14
+        ];
+      };
+    });
+}
